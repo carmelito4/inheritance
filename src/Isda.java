debad4f200bb70cc012ac1902662a09b2e24b47e
carmelito4 <claridocarmelito52@gmail.com>
2023-11-09 18:44:57 +0800

    added Isda file

---
+++ src/Isda.java
@@ -0,0 +1,17 @@
+public class Isda {
+    
+    String name;
+    
+    Isda (String name){
+        
+    }
+    void langoy(){
+        System.out.println(name + "Ni langoy");
+        
+    }
+    void salom(){
+        System.out.println(name + "Ni salom");
+    }
+    
+}
+
