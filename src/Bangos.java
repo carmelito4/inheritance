587adf6b649029090f725d943e26474ce65ce3fd
carmelito4 <claridocarmelito52@gmail.com>
2023-11-09 18:51:09 +0800

    added Bangos file

---
+++ src/Bangos.java
@@ -0,0 +1,10 @@
+public class Bangos extends Isda{
+    Bangos(String name){
+        super(name);
+        
+    }
+    void Langoy(){
+        System.out.println(name + "Ni langoy");
+    }
+    }
+
