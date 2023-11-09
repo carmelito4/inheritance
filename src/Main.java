b7238f49d02d481df7ec10b166bbb7408cfc140e
carmelito4 <claridocarmelito52@gmail.com>
2023-11-09 18:41:44 +0800

    add main java file

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  
+        public static void main(String[] args){
+            
+            Bangos myBangos= new Bangos("Edoy");
+            
+            myBangos.langoy();
+            myBangos.salom();
+            
+            
+        }
+        
+        
+        
+}
